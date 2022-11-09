58489178b14a048384d80f3d995496673777b82e
Leo <47742726+leonid1123@users.noreply.github.com>
2022-11-09 15:11:53 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+Console.WriteLine("Добро пожаловать в программу шифрования по методу ШИФР ЦЕЗАРЯ");
+Console.WriteLine("Введите фразу для шифрования и нажмите ENTER");
+string userInput = Console.ReadLine();
+Console.WriteLine("Укажите сдвиг шифра в виде целого числа и нажмите ENTER");
+int sdvig = int.Parse(Console.ReadLine());
+string alfavit = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+string newAlfavit = "";
+for (int i = 0; i < alfavit.Length; i++) {
+    int j = i + sdvig;
+    if (j > 32) {
+        j = j - 33;
+    }
+    newAlfavit += alfavit[j];
+}
+Console.WriteLine(alfavit);
+Console.WriteLine(newAlfavit);
+string shifr = "";
+for (int i = 0; i < userInput.Length; i++) {
+    int letterNumber = alfavit.IndexOf(userInput[i]);
+    Console.Write(newAlfavit[letterNumber]);
+    shifr+=newAlfavit[letterNumber];
+}
+File.WriteAllText("res.txt",shifr);
