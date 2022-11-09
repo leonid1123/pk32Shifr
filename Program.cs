661301823d5d88005ac8d8499d437d33ba956f1e
Leo <47742726+leonid1123@users.noreply.github.com>
2022-11-09 16:03:26 +0300

    commented

--- Program.cs
+++ Program.cs
@@ -1,24 +1,32 @@
-﻿using System.IO;
+﻿//ссылка на репозитарий https://github.com/leonid1123/pk32Shifr
+using System.IO; //нужно для записи в файл
 Console.WriteLine("Добро пожаловать в программу шифрования по методу ШИФР ЦЕЗАРЯ");
 Console.WriteLine("Введите фразу для шифрования и нажмите ENTER");
 string userInput = Console.ReadLine();
 Console.WriteLine("Укажите сдвиг шифра в виде целого числа и нажмите ENTER");
 int sdvig = int.Parse(Console.ReadLine());
-string alfavit = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+string alfavit = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"; //исходный алфавит
+//формирование алфавита с указанным сдвигом
 string newAlfavit = "";
 for (int i = 0; i < alfavit.Length; i++) {
     int j = i + sdvig;
-    if (j > 32) {
+    if (j > 32) {//проверка, если дошли до конца исходного алфавита
         j = j - 33;
     }
     newAlfavit += alfavit[j];
 }
-Console.WriteLine(alfavit);
-Console.WriteLine(newAlfavit);
+Console.WriteLine("Исходный алфавит:{0}",alfavit);
+Console.WriteLine("Алфавит со сдвигом {0}:{1}",sdvig,newAlfavit);
+//Формирование зашифрованного слова. Взять буквы исходного слова поочереди
+//найти их номер в исходном алфавите и взять букву с таким же номером в зашифрованном алфавите
 string shifr = "";
 for (int i = 0; i < userInput.Length; i++) {
     int letterNumber = alfavit.IndexOf(userInput[i]);
     Console.Write(newAlfavit[letterNumber]);
     shifr+=newAlfavit[letterNumber];
 }
-File.WriteAllText("res.txt",shifr);
+//Записать полученное слово и сдвиг в файл
+//Write - перезаписывает файл, Append - дописывает
+File.WriteAllText("res.txt",sdvig.ToString());
+File.AppendAllText("res.txt",shifr);
+
